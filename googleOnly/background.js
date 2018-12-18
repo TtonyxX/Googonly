71d277d5b822c9ef192d410ad0a2b6c11c463b79
Tony Xin <32372239+TtonyxX@users.noreply.github.com>
2018-12-18 14:34:18 -0800

    Added files

---
+++ googleOnly/background.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var host = "http://www.google.com";
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details) {
+         return {redirectUrl: host + details.url.match(/^https?:\/\/[^\/]+([\S\s]*)/)[1]};
+    },
+    {
+        urls: [
+            "*://yahoo.com/*",
+            "*://ask.com/*",
+            "*://baidu.com/*",
+            "*://duckduckgo.com/*"
+        ],
+        types: ["main_frame", "sub_frame", "stylesheet", "script", "image", "object", "xmlhttprequest", "other"]
+    },
+    ["blocking"]
+);
